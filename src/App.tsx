883f875f0a9631e4b0d2fc32fad616018eda74b6
Atabek Otepbergenov <atabek122002@gmail.com>
2023-07-06 14:43:57 +0300

    React Eslint✨

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './assets/styles/App.scss';
+
+const App: React.FC = () => <div className="app">App</div>;
+
+export { App };
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+
+import { App } from './App';
+
+import 'antd/dist/reset.css';
+import './assets/styles/base/_reset.scss';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
